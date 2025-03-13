5d1c7c3a6bb6e9060400ecb80517f045cda886eb
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-03-13 18:07:02 +0700

    w
    w

--- Assets/Rersoucer/Enemy/Boss/Script/BossController.cs
+++ Assets/Rersoucer/Enemy/Boss/Script/BossController.cs
@@ -155,7 +155,7 @@ public class BossController : MonoBehaviour
                 }
                 break;
             case EnemyState.Skill:
-                agent.isStopped = true;
+                agent.isStopped = false;
                 agent.stoppingDistance = rangeAttack;
 
                 break;

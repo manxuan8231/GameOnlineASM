1e1723ac146193524bd153fcad7335a6c388e5ff
PNGPhong <phongpngps40540@gmail.com>
2025-03-26 01:40:06 +0700

    zombie

--- Assets/RessotPhong/ScriptP/ZombieSpawner..cs
+++ Assets/RessotPhong/ScriptP/ZombieSpawner..cs
@@ -1,22 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using Fusion;
 
-public class ZombieSpawner : NetworkBehaviour
+public class SummonPrefabOnSpawn : NetworkBehaviour
 {
-    public GameObject zombiePrefab;
-    public Transform[] spawnPoints;
+    public GameObject prefabToSummon; // Prefab cần triệu hồi
+    public Transform spawnPoint; // Vị trí xuất hiện của prefab
+    private float spawnTime; // Thời gian player xuất hiện
 
     public override void Spawned()
     {
-        if (Object.HasStateAuthority) // Chỉ server hoặc host spawn zombie
+        if (Object.HasStateAuthority) // Chỉ chủ sở hữu mới thực hiện triệu hồi
         {
-            SpawnZombie();
+            spawnTime = Time.time; // Lưu lại thời gian player vào game
+            Debug.Log($"Player spawned at time: {spawnTime:F2} seconds");
+
+            StartCoroutine(SummonAfterDelay(2f)); // Triệu hồi sau 2 giây
         }
     }
 
-    void SpawnZombie()
+    private IEnumerator SummonAfterDelay(float delay)
     {
-        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-        NetworkObject zombie = Runner.Spawn(zombiePrefab, spawnPoint.position, Quaternion.identity);
+        yield return new WaitForSeconds(delay);
+
+        if (Runner.IsServer || Runner.IsSharedModeMasterClient) // Chỉ Server/MasterClient mới tạo Network Object
+        {
+            Vector3 spawnPosition = spawnPoint ? spawnPoint.position : transform.position + Vector3.forward * 2; // Vị trí spawn
+            Runner.Spawn(prefabToSummon, spawnPosition, Quaternion.identity);
+
+            Debug.Log($"Prefab spawned at position: {spawnPosition} (Time: {Time.time:F2} seconds)");
+        }
     }
 }

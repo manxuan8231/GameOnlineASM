d4a4e1ff2bc34cc5d4f3991c8d6b57da99645929
manxuan8231 <lydangxuanman05@gmail.com>
2025-04-13 23:23:24 +0700

    dw

--- Assets/Rersoucer/Car/Prefab/Car.cs
+++ Assets/Rersoucer/Car/Prefab/Car.cs
@@ -9,7 +9,7 @@ public class Car : NetworkBehaviour
 
     public float countRepair { get; set; }
     public TextMeshProUGUI textRepair;
-
+    public GameObject buttonF;
     public void OnRepairChanged()
     {
         textRepair.text = $"{countRepair}/{4}";
@@ -18,12 +18,30 @@ public class Car : NetworkBehaviour
     {
         if (other.CompareTag("Player"))
         {
-           
+            buttonF.SetActive(true);
+            PlayerProperties playerProperties = FindAnyObjectByType<PlayerProperties>();
+            if (Input.GetKeyDown(KeyCode.F) && playerProperties.countRepair > 0)
+            {
+                countRepair += 1;
+                textRepair.text = $"{countRepair}/{4}";
+                countRepair = Mathf.Clamp(countRepair, 0, 4);
+            }
         }
+        else
+        {
+            buttonF.SetActive(false);
+        }
+    }
+    public override void Spawned()
+    {
+        base.Spawned();
+        OnRepairChanged();
+
     }
     public void Start()
     {
         countRepair = 0;
         textRepair.text = $"{countRepair}/{4}";
+        buttonF.SetActive(false);
     }
 }
--- Assets/Rersoucer/ScriptOnline/PlayerProperties.cs
+++ Assets/Rersoucer/ScriptOnline/PlayerProperties.cs
@@ -75,6 +75,23 @@ public class PlayerProperties : NetworkBehaviour
     {
         Name = name;
     }
+
+    [Networked, OnChangedRender(nameof(OnChangeRepair))]
+    public float countRepair { get; set; }
+    public TextMeshProUGUI textRepair;
+    public void OnChangeRepair()
+    {
+        if (Object.HasInputAuthority)
+        {
+            textRepair.enabled = true;
+            textRepair.text = $"Repair: {countRepair}";
+            countRepair = Mathf.Clamp(countRepair, 0, 4);
+        }
+        else
+        {
+            textRepair.enabled = false;
+        }    
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Monster") || other.gameObject.CompareTag("Boss"))
@@ -87,13 +104,26 @@ public class PlayerProperties : NetworkBehaviour
                 networkRunner.Despawn(networkObject);
             }
         }
+        else if (other.gameObject.CompareTag("Box"))
+        {
+            if (Object.HasInputAuthority)
+            {
+                countRepair += 1;
+                textRepair.text = $"Repair: {countRepair}";
+                countRepair = Mathf.Clamp(countRepair, 0, 4);
+               
+                Destroy(other.gameObject);
+            }
+        }
     }
-
     void Start()
     {
         maxHealth = 100;
         currentHealth = maxHealth;
         textHealth.text = $"{currentHealth}/{maxHealth}";
+
+        countRepair = 0;
+        textRepair.text = $"Repair: {countRepair}";
     }
 
 
